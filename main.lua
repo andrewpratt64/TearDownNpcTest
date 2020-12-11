8ed511010cda13047e86fc0a163e95768a104c50
andrewpratt64 <adpratt@yahoo.com>
2020-12-10 19:55:53 -0500

    Added head, etc
    -Head now moves independently from torso
    
    - Npc dies when head is broken
    
    - Improved npc model
    
    - Improved physics

--- main.lua
+++ main.lua
@@ -23,6 +23,11 @@ local function QuatLookAtOnYaw(eye, target)
 end
 
 
+local function MakeBodyLookAt(body, target)
+	local bodyPos = GetBodyTransform(body).pos;
+	SetBodyTransform( body, Transform( bodyPos, QuatLookAt(bodyPos, target) ) );
+end
+
 local function MakeBodyLookAtOnYaw(body, target)
 	local bodyPos = GetBodyTransform(body).pos;
 	SetBodyTransform( body, Transform( bodyPos, QuatLookAtOnYaw(bodyPos, target) ) );
@@ -38,55 +43,130 @@ local function OffsetBodyPos(body, offset)
 end
 
 
+local function isNpcBodyValid(npc)
+	return (IsHandleValid(npc.torsoBody) and IsHandleValid(npc.headBody));
+end
 
-function updateNpcBody(body)
-	-- Get transform for npc and the player
-	local t = GetBodyTransform(body);
-	local plyT = GetPlayerTransform();
-	
-	-- Make npc face the player
-	MakeBodyLookAtOnYaw(body, plyT.pos);
-	
-	-- Store npc's direction from itself to the player as a normalized vector
-	local dir = VecNormalize(VecSub(plyT.pos, t.pos));
-	
-	foo = VecDiff(t.pos, plyT.pos);
-	
-	-- If the npc is far enough from the player that it should move...
-	if (VecDiff(t.pos, plyT.pos) > 1.1) then
-		-- Move npc forward
-		--AddBodyVelocity(body, VecScale(dir, 0.25));
-		OffsetBodyPos( body, VecScale( VecSwizzleXZ(dir), 0.1 ) );
-	end
+local function OffsetNpcPos(npc, offset)
+	OffsetBodyPos(npc.torsoBody, offset);
+	OffsetBodyPos(npc.headBody, offset);
+end
+
+local function killNpc(npc)
+	SetBodyDynamic(npc.torsoBody, true);
+	SetBodyDynamic(npc.headBody, true);
+	npc.bAlive = false;
 end
 
 
 
-function init()
-	foo = "nil";
-	npcBody = nil;
-	npcShape = FindShape("npc_torso", true);
+function updateNpc(npc)
+	-- Get transform for npc, player, and the camera
+	local torsoT = GetBodyTransform(npc.torsoBody);
+	local headT = GetBodyTransform(npc.headBody);
+	local plyT = GetPlayerTransform();
+	local camT = GetCameraTransform();
 	
-	if (npcShape ~= 0) then
-		npcBody = GetShapeBody(npcShape);
-		if (IsHandleValid(npcBody)) then
-			SetBodyDynamic(npcBody, true);
+	
+	-- Handle npc death
+	if (IsBodyBroken(npc.headBody) == true) then
+		killNpc(npc);
+		
+		-- Grab all bodies that *probably* came from the npc's head
+		local headBoundsMin, headBoundsMax = GetBodyBounds(npc.headBody);
+		QueryRequire("physical dynamic small");
+		--QueryAabbBodies( VecSub( headT.pos, Vec(0.2, 0, 0.2) ), VecAdd( headT.pos, Vec(0.2, 0.3, 0.2) ) );
+		local potentialHeadBodies = QueryAabbBodies(headBoundsMin, headBoundsMax);
+		
+		-- Give physics and a small impulse to the head debris to prevent floating pieces
+		for _, headDebrisBody in pairs(potentialHeadBodies) do
+			SetBodyDynamic(headDebrisBody, true);
+			
+			local headDebrisT = GetBodyTransform(headDebrisBody);
+			ApplyBodyImpulse( headDebrisBody, VecSub( headDebrisT.pos, Vec(0, 0.01, 0) ), Vec(0, 0.01, 0) );
 		end
+	end	
+	
+	-- Make npc face the player
+	MakeBodyLookAtOnYaw(npc.torsoBody, plyT.pos);
+	MakeBodyLookAt(npc.headBody, camT.pos);
+	
+	-- Store npc's direction from itself to the player as a normalized vector
+	local dir = VecNormalize(VecSub(plyT.pos, torsoT.pos));
+	
+	-- If the npc is far enough from the player that it should move...
+	if (VecDiff(torsoT.pos, plyT.pos) > 1.1) then
+		-- Move npc forward
+		--AddBodyVelocity(body, VecScale(dir, 0.25));
+		OffsetNpcPos( npc, VecScale( VecSwizzleXZ(dir), 0.1 ) );
+	end
+end
+
+
+function npc_init()
+	-- TODO: Implement an npc class
+	npc = {
+		torsoBody = nil;
+		torsoShape = nil;
+		
+		headBody = nil;
+		headShape = nil;
+		
+		neckJoint = nil;
+		
+		bAlive = true;
+	};
+	
+	--DebugPrint("body found: " .. FindBody("npc", true));
+	
+	npc.torsoShape = FindShape("npc_torso", true);
+	npc.headShape = FindShape("npc_head", true);
+	
+	if (npc.torsoShape ~= 0) then
+		npc.torsoBody = GetShapeBody(npc.torsoShape);
+		if (IsHandleValid(npc.torsoBody)) then
+			SetBodyDynamic(npc.torsoBody, false);
+		end
+	end
+	
+	if (npc.headShape ~= 0) then
+		npc.headBody = GetShapeBody(npc.headShape);
+		if (IsHandleValid(npc.headBody)) then
+			SetBodyDynamic(npc.headBody, false);
+			
+			npc.neckJoint = FindJoint("npc_joint_neck", true);
+		end
+	end
+end
+
+function init()
+	local status, err = pcall(npc_init);
+	
+	if (status == false) then
+		DebugPrint("LUA ERROR IN MOD npc IN FUNCTION init: " .. err);
 	end
 end
 	
 	
 function tick()
+	
 end
 
 
+function npc_update()
+	if (isNpcBodyValid(npc) and npc.bAlive == true) then
+		updateNpc(npc);
+	end
+end
+
 function update()
-	if (IsHandleValid(npcBody)) then
-		updateNpcBody(npcBody);
+	local status, err = pcall(npc_update);
+	
+	if (status == false) then
+		--DebugPrint("LUA ERROR IN MOD npc IN FUNCTION update: " .. err);
 	end
 end
 
 
 function draw()
-	DebugWatch("foo", foo);
 end
